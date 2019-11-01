8de9588c719e117ab62f74f2f5f0a70e238c3b36
Sam Blundell <SamB1990@users.noreply.github.com>
2019-11-01 16:08:55 +0000

    Read commands

---
+++ src/Read/ReadCommands.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Console.CommandHandler;
+
+namespace System
+{
+    public static partial class ConsoleX
+    {
+        public static IEnumerable<IExecutableCommand> ReadCommands()
+        {
+            var input = Console.ReadLine();
+
+            foreach (Match match in Regex.Matches(input, @"(--\w*|-\w*)+\w*[^-]*", RegexOptions.Multiline))
+            {
+                var commandString = match.Groups[1].ToString();
+                yield return new ExecutableCommand()
+                {
+                    Command = new Command(commandString),
+                    Arguments = GetArguments(match.Groups[0].ToString().Replace(commandString, ""))
+                };
+            }
+        }
+
+        public static IEnumerable<string> GetArguments(string argumentString)
+        {
+            if (string.IsNullOrWhiteSpace(argumentString))
+                return new string[] { };
+
+            var match = Regex.Match(argumentString.Trim(), @"(?<=\[).+?(?=\])");
+
+            return !match.Success ? new string[] { argumentString } : match.Value.Split(',');
+        }
+    }
+}
---
+++ src/Write/WriteLines.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static partial class ConsoleX
+    {
+        public static void WriteLines(IEnumerable<string> messages, ConsoleX.MessageStatus status = ConsoleX.MessageStatus.Default, ConsoleX.LogLevel level = ConsoleX.LogLevel.none)
+        {
+            foreach (var message in messages)
+            {
+                ConsoleX.WriteLine(message, status, level);
+            }
+        }
+    }
+}
